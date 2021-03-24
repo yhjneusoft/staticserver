7bc632119f5570db7233f6a4736894275bdfec9f
yhjneusoft <57080526+yhjneusoft@users.noreply.github.com>
2021-03-24 16:59:02 +0800

    1

--- piano/js/index.js
+++ piano/js/index.js
@@ -3,12 +3,11 @@ let liArr = document.getElementsByTagName('ul')[0].getElementsByTagName('li');
 for (let i = 0; i < liArr.length; i++) {
     liArr[i].ontouchstart = function () {
         this.style.boxShadow = 'none';
-        makeSound(i);
+        makeSound(6-i);    //低音在前
     }
     liArr[i].ontouchend = function () {
         this.style.boxShadow = '#666 0px 0px 10px';
     }
 }
-
 
 
