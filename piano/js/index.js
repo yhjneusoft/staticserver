be0aef8661c78ec778ccdb3c85664db92105e4ba
yhjneusoft <57080526+yhjneusoft@users.noreply.github.com>
2021-03-24 14:04:52 +0800

    7

--- piano/js/index.js
+++ piano/js/index.js
@@ -1,8 +1,13 @@
 let liArr = document.getElementsByTagName('ul')[0].getElementsByTagName('li');
 
+let audio = document.createElement('audio');
+document.body.appendChild(audio);
+
 for(let i=0;i<liArr.length;i++){
     liArr[i].ontouchstart = function(){
         this.style.boxShadow = 'none';
+        audio.src = './music/'+(i+1)+'.MP3';
+        audio.play();
     }
     liArr[i].ontouchend = function(){
         this.style.boxShadow = '#666 0px 0px 10px';
