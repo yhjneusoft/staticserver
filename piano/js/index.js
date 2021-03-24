a5273280d8f69f80829326fc2f4da6e080484ee4
yhjneusoft <57080526+yhjneusoft@users.noreply.github.com>
2021-03-24 13:44:46 +0800

    2

---
+++ piano/js/index.js
@@ -0,0 +1,10 @@
+let liArr = document.getElementsByTagName('ul')[0].getElementsByTagName('li');
+
+for(let i=0;i<liArr.length;i++){
+    liArr[i].ontouchstart = function(){
+        this.style.boxShadow = 'none';
+    }
+    liArr[i].ontouchend = function(){
+        this.style.boxShadow = '#666 0px 0px 10px';
+    }
+}
