e424fb62439e6caa78e46956f6d9a2eeaf550c4a
yhjneusoft <57080526+yhjneusoft@users.noreply.github.com>
2021-03-24 15:43:28 +0800

    1

--- piano/js/makeSound.js
+++ piano/js/makeSound.js
@@ -1,26 +1,27 @@
-function makeSound(index) {
-    let sounds = [130, 147, 165, 175, 196, 220, 246, 262, 294, 330, 349, 392, 440, 494,
-        523, 587, 659, 698, 784, 880, 988, 1047];
-    // 获得音频上下文
-    let ctx = new AudioContext();
+let ctx;
+let sounds = [130, 147, 165, 175, 196, 220, 246, 262, 294, 330, 349, 392, 440, 494,
+    523, 587, 659, 698, 784, 880, 988, 1047];
 
-    //得到音频振荡器
-    let osc = ctx.createOscillator();
-    //得到音量控制对象
-    let g = ctx.createGain();
-
-    // 连接振荡器和音量控制对象
-    osc.connect(g);
-    osc.type = 'sine';
-    osc.frequency.value = sounds[index];
-
-    let duration = 1;
-
-    g.connect(ctx.destination);
-    g.gain.value = 0;
-    osc.start();
-    g.gain.linearRampToValueAtTime(0.6, ctx.currentTime + 0.01)
-
+function setContent() {
+    if (!ctx) {
+        ctx = new AudioContext()
+    }
+}
+function makeSound(index) {             // 钢琴传入是钢琴的第几个按键
+    setContent();                       // 获得音频上下文
+    var osc = ctx.createOscillator();   //获得音频振荡器
+    var g = ctx.createGain();           //得到音量控制对象
+    osc.connect(g);                     // 连接振荡器和音量控制对象
+    osc.type = 'sine';                  //设置波形
+    osc.frequency.value = sounds[index];//对应钢琴不同键的不同频率
+    var duration = 1;                   //控制时间
+    g.connect(ctx.destination);         //连接扬声器
+    g.gain.value = 0;                   //初始音高为0
+    osc.start();                        //从当前开始发生
+    //从当前时间到0.01s后，音高从0~0.6
+    g.gain.linearRampToValueAtTime(0.6, ctx.currentTime + 0.01);
+    //当前时间duration后停止
     osc.stop(ctx.currentTime + duration);
-    g.gain.exponentialRampToValueAtTime(0.01, ctx.currentTime + duration)
+    //从0.6~0.01
+    g.gain.exponentialRampToValueAtTime(0.01, ctx.currentTime + duration);
 }
